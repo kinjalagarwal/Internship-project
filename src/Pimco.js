19c3bf099616a7e323c849bd6b731bd55bc8ba40
kinjalagarwal <86708396+kinjalagarwal@users.noreply.github.com>
2021-07-19 16:54:23 +0530

    Add files via upload

---
+++ src/Pimco.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Pimco = () => {
+    return (
+        <div className="Pimco">
+            <h1>Pimco</h1>
+        </div>
+    );
+};
+
+export const TeamStructure = () => {
+    return (
+        <div className="Pimco">
+            <h1>TeamStructure</h1>
+        </div>
+    );
+};
+
+export const TeamDetails = () => {
+    return (
+        <div className="Pimco">
+            <h1>TeamStructure</h1>
+        </div>
+    );
+};
---
+++ src/ServiceOps.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServiceOps = () => {
+    return (
+        <div className='home'>
+            <h1>ServiceOps</h1>
+            
+        </div>
+    );
+};
+
+export default ServiceOps;
